0ca2d3df234b0075f45957f8b56a47854b4b39e2
bonustrack <fabien@bonustrack.co>
2022-08-04 21:48:09 +0700

    Add vue config

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-module.exports = require('./.eslintrc.json');
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended'],
+  plugins: ['prettier', '@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        singleQuote: true,
+        printWidth: 100,
+        tabWidth: 2,
+        trailingComma: 'none',
+        arrowParens: 'avoid'
+      }
+    ],
+    'no-console': 'off',
+    'prefer-template': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+};
---
+++ vue.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier'
+  ],
+  ignorePatterns: ['/node_modules/**/*.*'],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/no-v-html': 'off'
+  },
+  globals: {
+    $ref: 'readonly',
+    defineProps: 'readonly',
+    defineEmits: 'readonly'
+  }
+};
