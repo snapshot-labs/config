55712f9cb2c6598e5cbff9a05db3bdac4eea9f3a
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-11-22 20:18:56 +0100

    feat: add Vue config (#5)

---
+++ packages/vue/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ['plugin:vue/vue3-recommended', '@snapshot-labs/eslint-config-base'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/no-v-html': 'off'
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn'
+      }
+    }
+  ]
+};
