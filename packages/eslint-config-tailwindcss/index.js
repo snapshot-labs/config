5c721a1009417399da50bed807c3f8937f08646b
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-07-28 23:46:24 +0900

    fix: disable rules with big effects

--- packages/eslint-config-tailwindcss/index.js
+++ packages/eslint-config-tailwindcss/index.js
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: ['plugin:tailwindcss/recommended']
+  extends: ['plugin:tailwindcss/recommended'],
+  rules: {
+    'tailwindcss/classnames-order': 'off',
+    'tailwindcss/no-unnecessary-arbitrary-value': 'off',
+    'tailwindcss/no-custom-classname': 'off'
+  }
 };
