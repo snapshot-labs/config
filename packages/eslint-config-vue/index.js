1db38604c18604496b734cd46603ff4b65e2c949
Chaitanya <yourchaitu@gmail.com>
2024-08-01 16:35:11 +0530

    fix: Add lint rule to enforce camelCase event name (#25)
    * fix: Add lint rule to enforce camelCase event name
    
    * v0.1.0-beta.18

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -3,7 +3,8 @@ module.exports = {
   rules: {
     'vue/multi-word-component-names': 'off',
     'vue/require-default-prop': 'off',
-    'vue/no-v-html': 'off'
+    'vue/no-v-html': 'off',
+    'vue/custom-event-name-casing': ['error', 'camelCase']
   },
   overrides: [
     {
