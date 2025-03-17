cb725401cde75e147324f296bc418d880a52e99a
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-03-17 13:37:53 +0100

    fix: remove no-unused-vars override in vue (#29)
    no-unused-vars was actually already set to error, we just
    override it in vue config.

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -12,10 +12,6 @@ module.exports = {
       parser: 'vue-eslint-parser',
       parserOptions: {
         parser: '@typescript-eslint/parser'
-      },
-      rules: {
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': 'warn'
       }
     }
   ]
